d1691cf8d324922bb4b749151d6099ece42bd90f
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2020-08-21 17:38:38 -0300

    commit

--- Exercicios_Aula/aula05/if_parte1/exercicio09.py
+++ Exercicios_Aula/aula05/if_parte1/exercicio09.py
@@ -5,8 +5,20 @@ a = int(input("Informe um número: "))
 b = int(input("Informe um número: "))
 c = int(input("Informe um número: "))
 
-l = [a, b, c]
+if a > b:
+    aux = a
+    a = b
+    b = aux
+if a > c:
+    aux = a 
+    a = c
+    c = aux
+if b > c:
+    aux = b
+    b = c
+    c = aux
 
-print(sorted(l))
-
+print(a)
+print(b)
+print(c)
 
--- Exercicios_Aula/aula05/if_parte1/exercicio10.py
+++ Exercicios_Aula/aula05/if_parte1/exercicio10.py
@@ -6,6 +6,19 @@ a = int(input("Informe um número: "))
 b = int(input("Informe um número: "))
 c = int(input("Informe um número: "))
 
-l = [a, b, c]
+if a < b:
+    aux = a
+    a = b
+    b = aux
+if a < c:
+    aux = a 
+    a = c
+    c = aux
+if b < c:
+    aux = b
+    b = c
+    c = aux
 
-print(sorted(l, reverse = True))
+print(a)
+print(b)
+print(c)
