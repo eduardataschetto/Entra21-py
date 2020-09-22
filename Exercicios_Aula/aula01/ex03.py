43fba4fe6cfe5ae9416b156e68ba60e42d0a8a0d
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2020-09-22 19:19:20 -0300

    commit

--- Exercicios_Aula/aula01/ex03.py
+++
@@ -1,5 +0,0 @@
-#pedir 2 números inteiros e fazer a soma deles
-a = int(input("Informe um número: "))
-b = int(input("Informe outro número: "))
-
-print(f'O resultado da soma entre {a} e {b} é igual a {a+b}.')
